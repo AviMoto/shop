6dd637a883beb84b0cb7bb7500e4779dda022ece
Mass <fubar909@hotmail.com>
2014-08-18 19:26:37 +0300

    Add needed javascripts libraries and frameworks

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE >
 <html>
 	<head>
-		
+		<meta charset="UTF-8">
+		<title>Class Shop</title>
+		<script src="assets/js/lib/angular.min.js"></script>
+		<script src="assets/js/lib/jquery-ui.min.js"></script>
+		<script src="assets/js/lib/jquery-ui.min.js"></script>
 	</head>
 	<body>
 		<h1>
